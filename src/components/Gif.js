43ddb4d5757d8ec1137dc806a1cf95161ffbd676
Leandro <lecorvalan2@gmail.com>
2022-08-02 02:30:08 -0300

    Gif.css - Agregando algunos estilos

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,9 +1,11 @@
+import './Gif.css'
+
 export default function Gif({ title, id, url}) {
   return (
-    <div>
+    <a href='' className="Gif">
         <h4>{title}</h4>
         <small>{id}</small>
         <img alt={title} src={url} />
-    </div>
+    </a>
   )
 }

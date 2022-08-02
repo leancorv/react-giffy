67e9cf401fec30540996f3d651ae95a70559fda9
Leandro <lecorvalan2@gmail.com>
2022-08-02 16:11:52 -0300

    useGifs - Creando Custom Hook

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,28 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Link, useLocation } from "wouter"
+import ListOfGifs from "../../components/ListOfGifs"
+import getGifs from "../../services/getGifs"
 
 const POPULAR_GIFS = ["Matrix", "Chile", "Colombia", "Ecuador"]
 
 export default function Home() {
     const [keyword, setKeyword] = useState('')
     const [path, pushLocation] = useLocation()
+
+    const [loading, setLoading] = useState(false)
+    const [gifs, setGifs] = useState([])
+    
+    // <----------------------
+    useEffect(function() {
+      setLoading(true)
+      getGifs({ keyword: 'Rick' })
+        .then(gifs => {
+          setGifs(gifs)
+          setLoading(false)
+        })
+    }, [keyword])
+
+    // <----------------------
 
     const handleSubmit = e => {
         e.preventDefault()
@@ -23,6 +40,8 @@ export default function Home() {
         <form onSubmit={handleSubmit}>
             <input placeholder="Search a gif here..." onChange={handleChange} type="text" value={keyword} />
         </form>
+        <h3 className="App-title">Última búsqueda</h3>
+        <ListOfGifs gifs={gifs} />
         <h3 className="App-title">Los gifs más populares</h3>
         <ul>
             {

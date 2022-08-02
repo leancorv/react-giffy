0c81d359f948e7a545885eb7289055368097cf1b
Leandro <lecorvalan2@gmail.com>
2022-08-02 15:22:57 -0300

    Search - Haciendo el search y usando UseLocation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,28 @@
-import { Link } from "wouter"
+import { useState } from "react"
+import { Link, useLocation } from "wouter"
 
 const POPULAR_GIFS = ["Matrix", "Chile", "Colombia", "Ecuador"]
 
 export default function Home() {
+    const [keyword, setKeyword] = useState('')
+    const [path, pushLocation] = useLocation()
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        // navegar a otra ruta
+        pushLocation(`/search/${keyword}`)
+        console.log(keyword)
+    }
+
+    const handleChange = e => {
+        setKeyword(e.target.value)
+    }
+
   return (
     <>
+        <form onSubmit={handleSubmit}>
+            <input placeholder="Search a gif here..." onChange={handleChange} type="text" value={keyword} />
+        </form>
         <h3 className="App-title">Los gifs más populares</h3>
         <ul>
             {
--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,5 +1,24 @@
-export default function SearchResults() {
-  return (
-    <div>index</div>
-  )
+import { useEffect, useState } from "react"
+import ListOfGifs from "../../components/ListOfGifs"
+import Spinner from "../../components/Spinner"
+import getGifs from "../../services/getGifs"
+
+export default function SearchResults({ params }) {
+  const { keyword } = params
+  const [loading, setLoading] = useState(false)
+  const [gifs, setGifs] = useState([])
+  
+  useEffect(function() {
+    setLoading(true)
+    getGifs({ keyword })
+      .then(gifs => {
+        setGifs(gifs)
+        setLoading(false)
+      })
+  }, [keyword])
+  return <>
+    {
+      loading ? <Spinner /> : <ListOfGifs gifs={gifs} />
+    }
+  </>
 }

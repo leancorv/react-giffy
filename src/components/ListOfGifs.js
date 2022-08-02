8de4ee7454f57ce743fd29240976526dbe484f23
Leandro <lecorvalan2@gmail.com>
2022-08-02 03:30:59 -0300

    ListOfGifs - Guardando un objeto en el state

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -4,24 +4,23 @@ import Gif from "./Gif";
 
 export default function ListOfGifs({ params }) {
     const { keyword } = params
-    const [loading, setLoading] = useState(false)
-
-    const [gifs, setGifs] = useState([])
+    const [gifs, setGifs] = useState(
+        { loading: false, results: []}
+    )
 
     useEffect(function () {
-        setLoading(true)
+        setGifs(actualGifs => ({loading: true, results: actualGifs.results}))
         getGifs({ keyword })
           .then(gifs => {
-            setGifs(gifs)
-            setLoading(false)
+            setGifs({loading: false, results: gifs })
         })
     }, [keyword])
 
-    if (loading) return <i>Cargando...⌚</i>
+    if (gifs.loading) return <i>Cargando...⌚</i>
 
   return <>
     {   
-        gifs.map(({id, title, url}) => 
+        gifs.results.map(({id, title, url}) => 
             <Gif 
                 id={id}
                 key={id}

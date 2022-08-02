3c4eca2cb8ae01d7a68c493038d7aa2efb9cefe0
Leandro <lecorvalan2@gmail.com>
2022-08-02 14:08:36 -0300

    App - Mejorando rutas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import './App.css';
-import ListOfGifs from './components/ListOfGifs';
-
+import ListOfGifs from './components/ListOfGifs/ListOfGifs';
 import { Link, Route } from "wouter"
+import Detail from './pages/Detail';
+import Home from './pages/Home';
+import SearchResults from './pages/SearchResults';
 
 function App() {
   return (
     <div className="App">
       <section className="App-content">
-        <h1>App</h1>
-        <Link to='/gif/colombia'>Gifs de Colombia</Link>
-        <Link to='/gif/ecuador'>Gifs de Ecuador</Link>
-        <Link to='/gif/chile'>Gifs de Chile</Link>
+        <Link to="/">
+          <img className='App-logo' alt='Giffy logo' src='/logo.png'/>
+        </Link>
+        <Route
+          component={Home}
+          path="/"
+        />
+        <Route
+          component={SearchResults}
+          path="/search/:keyword"
+        />
         <Route 
-          component={ListOfGifs}
-          path="/gif/:keyword"
+          component={Detail}
+          path="/gif/:id"
         />
       </section>
     </div>

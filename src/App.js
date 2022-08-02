a509c2bdf33a433da68bbb0bb3562aaf44a431d5
Leandro <lecorvalan2@gmail.com>
2022-08-02 03:09:16 -0300

    Wouter - Usando Link para que sea SPA

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import ListOfGifs from './components/ListOfGifs';
 
-import { Route } from "wouter"
+import { Link, Route } from "wouter"
 
 function App() {
   return (
     <div className="App">
       <section className="App-content">
         <h1>App</h1>
-        <a href='/gif/colombia'>Gifs de Colombia</a>
-        <a href='/gif/ecuador'>Gifs de Ecuador</a>
-        <a href='/gif/chile'>Gifs de Chile</a>
+        <Link to='/gif/colombia'>Gifs de Colombia</Link>
+        <Link to='/gif/ecuador'>Gifs de Ecuador</Link>
+        <Link to='/gif/chile'>Gifs de Chile</Link>
         <Route 
           component={ListOfGifs}
           path="/gif/:keyword"

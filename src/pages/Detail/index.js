23187baf5ffb1782a1ca4430dffd1713471d922d
Leandro <lecorvalan2@gmail.com>
2022-08-03 03:47:25 -0300

    useGlobalGifs - Creando custom hook de lectura

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,9 +1,8 @@
-import { useContext } from "react"
 import Gif from "../../components/Gif"
-import GifsContext from "../../context/GifsContext"
+import useGlobalGifs from "../../hooks/useGlobalGifs"
 
 export default function Detail({ params }) {
-  const {gifs} = useContext(GifsContext)
+  const gifs = useGlobalGifs()
 
   const gif = gifs.find(singleGif => 
     singleGif.id === params.id

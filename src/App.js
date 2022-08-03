237688fff3c0e8821c3a7ba28b964e6b912b13d3
Leandro <lecorvalan2@gmail.com>
2022-08-02 23:53:54 -0300

    StaticContext - usando Context estático

--- src/App.js
+++ src/App.js
@@ -3,28 +3,36 @@ import { Link, Route } from "wouter"
 import Detail from './pages/Detail';
 import Home from './pages/Home';
 import SearchResults from './pages/SearchResults';
+import StaticContext from './context/StaticContext';
 
 function App() {
   return (
-    <div className="App">
-      <section className="App-content">
-        <Link to="/">
-          <img className='App-logo' alt='Giffy logo' src='/logo.png'/>
-        </Link>
-        <Route
-          component={Home}
-          path="/"
-        />
-        <Route
-          component={SearchResults}
-          path="/search/:keyword"
-        />
-        <Route 
-          component={Detail}
-          path="/gif/:id"
-        />
-      </section>
-    </div>
+    <StaticContext.Provider value={
+      {
+        name: 'midudev',
+        suscribeteAlCanal: true
+      }
+    }>
+      <div className="App">
+        <section className="App-content">
+          <Link to="/">
+            <img className='App-logo' alt='Giffy logo' src='/logo.png'/>
+          </Link>
+          <Route
+            component={Home}
+            path="/"
+          />
+          <Route
+            component={SearchResults}
+            path="/search/:keyword"
+          />
+          <Route 
+            component={Detail}
+            path="/gif/:id"
+          />
+        </section>
+      </div>
+    </StaticContext.Provider>
   );
 }
 
--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,4 +1,8 @@
+import { useContext } from "react"
+import StaticContext from "../../context/StaticContext"
+
 export default function Detail({ params }) {
+  useContext(StaticContext)
   console.log(params.id)
   return (
     <h1>Gif con id {params.id}</h1>

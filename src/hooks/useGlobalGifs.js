cba75b914336832092173b571ee0ff1a35f98471
Leandro <lecorvalan2@gmail.com>
2022-08-03 03:51:28 -0300

    useGlobalGifs - Haciendo el código mas limpio

--- src/hooks/useGlobalGifs.js
+++ src/hooks/useGlobalGifs.js
@@ -2,6 +2,5 @@ import { useContext } from 'react'
 import GifsContext from "../context/GifsContext"
 
 export default function useGlobalGifs () {
-    const {gifs} = useContext(GifsContext)
-    return gifs
+    return useContext(GifsContext).gifs
 }

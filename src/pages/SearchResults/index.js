d99bef25752e8fc577cce8e1e856d1b5ecd68602
Leandro <lecorvalan2@gmail.com>
2022-08-05 17:15:13 -0300

    Callback - Añade dependencia

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -16,7 +16,7 @@ export default function SearchResults({ params }) {
 
   const debounceHandleNextPage = useCallback(debounce(
     () => setPage(prevPage => prevPage + 1), 200
-  ), [])
+  ), [setPage])
 
   useEffect(function() {
     console.log(isNearScreen)

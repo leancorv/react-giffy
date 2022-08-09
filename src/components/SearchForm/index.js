96b11d899e330981acaeedeae19c7dba249c603a
Leandro <lecorvalan2@gmail.com>
2022-08-09 17:51:49 -0300

    SearchForm - Se añade filtro de rating

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react'
 import { useLocation } from "wouter"
 import css from './SearchForm.module.css'
 
-function SearchForm({ onSubmit }) {
+const RATINGS = ['g', 'pg', 'pg-13', 'r']
+
+function SearchForm() {
     const [keyword, setKeyword] = useState('')
+    const [rating, setRating] = useState(RATINGS[0])
     const [_, pushLocation] = useLocation()
 
     const handleChange = evt => {
@@ -16,10 +19,25 @@ function SearchForm({ onSubmit }) {
         pushLocation(`/search/${keyword}`)
     }
 
+    const handleChangeRating = evt => {
+      setRating(evt.target.value)
+    }
+
   return (
     <form onSubmit={handleSubmit} className={css["c-search"]}>
       <button className={css["c-search-btn"]}>Buscar</button>
-      <input className={css["c-search-input"]} placeholder="Search a gif here..." onChange={handleChange} type='text' value={keyword} />
+      <input 
+        className={css["c-search-input"]} 
+        placeholder="Search a gif here..." 
+        onChange={handleChange} type='text' 
+        value={keyword} 
+      />
+      <select onChange={handleChangeRating} value={rating}>
+        <option disabled>Rating type</option>
+        {RATINGS.map(rating => (
+          <option key={rating}>{rating}</option>
+        ))}
+      </select>
     </form>
   )
 }

302696d327d3f05f03b7f716a00d8bfddf2502e3
Leandro <lecorvalan2@gmail.com>
2022-08-10 15:39:17 -0300

    Reducer sin switch y programación funcional

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -9,23 +9,21 @@ const ACTIONS = {
   UPDATE_RATING: 'update_rating'
 }
 
+const ACTIONS_REDUCERS = {
+  [ACTIONS.UPDATE_KEYWORD]: (state, action) => ({
+    ...state,
+    keyword: action.payload,
+    times: state.times + 1
+  }),
+  [ACTIONS.UPDATE_RATING]: (state, action) => ({
+    ...state,
+    rating: action.payload
+  }),
+}
+
 const reducer = (state, action) => {
-  switch (action.type) {
-    case ACTIONS.UPDATE_KEYWORD:
-      return {
-        ...state,
-        keyword: action.payload,
-        times: state.times + 1
-      }
-      
-    case ACTIONS.UPDATE_RATING:
-      return {
-        ...state,
-        rating: action.payload
-      }
-    default: 
-    return state
-  }
+  const actionReducer = ACTIONS_REDUCERS[action.type]
+  return actionReducer ? actionReducer(state, action) : state
 }
 
 function SearchForm({ initialKeyword = '', initialRating = 'g'}) {

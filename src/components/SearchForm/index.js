eeb392c60578635135937d1af9051ec082adc507
Leandro <lecorvalan2@gmail.com>
2022-08-10 15:11:56 -0300

    SearchForm - Se añaden mejoras al useReducer

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,38 +1,41 @@
-import React, { useReducer, useState } from 'react'
+import React, { useReducer } from 'react'
 import { useLocation } from "wouter"
 import css from './SearchForm.module.css'
 
 const RATINGS = ['g', 'pg', 'pg-13', 'r']
 
-const reducer = (state, param) => {
-  console.log(param)
-  return {
-    ...state,
-    keyword: param,
-    times: state.times + 1
+const reducer = (state, action) => {
+  if (action.type === 'update_keyword') {
+    return {
+      ...state,
+      keyword: action.payload,
+      times: state.times + 1
+    }
+  } else if (action.type === 'update_rating') {
+    return {
+      ...state,
+      rating: action.payload,
+      times: state.times + 1
+    }
   }
+
+  return state
 }
 
 function SearchForm({ initialKeyword = '', initialRating = 'g'}) {
-    // const [keyword, setKeyword] = useState(decodeURIComponent(initialKeyword));
-    const [rating, setRating] = useState(initialRating);
-    // const [times, setTimes] = useState(0)
 
     const [state, dispatch] = useReducer(reducer, {
       keyword: decodeURIComponent(initialKeyword),
+      rating: initialRating,
       times: 0
     })
 
-    const {keyword, times} = state
+    const {keyword, rating, times} = state
 
     const [_, pushLocation] = useLocation();
 
-    const updateKeyword = (keyword) => {
-      dispatch(keyword)
-    }
-
     const handleChange = evt => {
-      updateKeyword(evt.target.value)
+      dispatch({ type: 'update_keyword', payload: evt.target.value })
     }
 
     const handleSubmit = evt => {
@@ -42,7 +45,7 @@ function SearchForm({ initialKeyword = '', initialRating = 'g'}) {
     }
 
     const handleChangeRating = evt => {
-      setRating(evt.target.value)
+      dispatch({ type: 'update_rating', payload: evt.target.value })
     }
 
   return (

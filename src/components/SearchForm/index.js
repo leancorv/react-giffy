409beeccb9876a65bfe34166f31c47b29c4c89ca
Leandro <lecorvalan2@gmail.com>
2022-08-10 16:06:07 -0300

    Reducer - creando custom hook del reducer

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,45 +1,17 @@
-import React, { useReducer } from 'react'
+import React from 'react'
 import { useLocation } from "wouter"
 import css from './SearchForm.module.css'
+import useForm from './hook'
 
 const RATINGS = ['g', 'pg', 'pg-13', 'r']
 
-const ACTIONS = {
-  UPDATE_KEYWORD: 'update_keyword',
-  UPDATE_RATING: 'update_rating'
-}
-
-const ACTIONS_REDUCERS = {
-  [ACTIONS.UPDATE_KEYWORD]: (state, action) => ({
-    ...state,
-    keyword: action.payload,
-    times: state.times + 1
-  }),
-  [ACTIONS.UPDATE_RATING]: (state, action) => ({
-    ...state,
-    rating: action.payload
-  }),
-}
-
-const reducer = (state, action) => {
-  const actionReducer = ACTIONS_REDUCERS[action.type]
-  return actionReducer ? actionReducer(state, action) : state
-}
-
 function SearchForm({ initialKeyword = '', initialRating = 'g'}) {
-
-    const [state, dispatch] = useReducer(reducer, {
-      keyword: decodeURIComponent(initialKeyword),
-      rating: initialRating,
-      times: 0
-    })
-
-    const {keyword, rating, times} = state
+    const {keyword, rating, times, updateKeyword, updateRating} = useForm({ initialKeyword, initialRating })
 
     const [_, pushLocation] = useLocation();
 
     const handleChange = evt => {
-      dispatch({ type: ACTIONS.UPDATE_KEYWORD, payload: evt.target.value })
+      updateKeyword(evt.target.value)
     }
 
     const handleSubmit = evt => {
@@ -49,7 +21,7 @@ function SearchForm({ initialKeyword = '', initialRating = 'g'}) {
     }
 
     const handleChangeRating = evt => {
-      dispatch({ type: ACTIONS.UPDATE_RATING, payload: evt.target.value })
+      updateRating(evt.target.value)
     }
 
   return (

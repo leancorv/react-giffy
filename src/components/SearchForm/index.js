2054907a4b732d0b4c61f90c722c719c9ca4d91d
Leandro <lecorvalan2@gmail.com>
2022-08-10 03:07:16 -0300

    Se implementa useReducer

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,16 +1,38 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
 import { useLocation } from "wouter"
 import css from './SearchForm.module.css'
 
 const RATINGS = ['g', 'pg', 'pg-13', 'r']
 
+const reducer = (state, param) => {
+  console.log(param)
+  return {
+    ...state,
+    keyword: param,
+    times: state.times + 1
+  }
+}
+
 function SearchForm({ initialKeyword = '', initialRating = 'g'}) {
-    const [keyword, setKeyword] = useState(decodeURIComponent(initialKeyword));
+    // const [keyword, setKeyword] = useState(decodeURIComponent(initialKeyword));
     const [rating, setRating] = useState(initialRating);
+    // const [times, setTimes] = useState(0)
+
+    const [state, dispatch] = useReducer(reducer, {
+      keyword: decodeURIComponent(initialKeyword),
+      times: 0
+    })
+
+    const {keyword, times} = state
+
     const [_, pushLocation] = useLocation();
 
+    const updateKeyword = (keyword) => {
+      dispatch(keyword)
+    }
+
     const handleChange = evt => {
-        setKeyword(evt.target.value)
+      updateKeyword(evt.target.value)
     }
 
     const handleSubmit = evt => {
@@ -38,6 +60,7 @@ function SearchForm({ initialKeyword = '', initialRating = 'g'}) {
           <option key={rating}>{rating}</option>
         ))}
       </select>
+      <small>{times}</small>
     </form>
   )
 }

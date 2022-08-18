c1f6316819d363bf976558d9b01f292a30310c3e
Leandro <lecorvalan2@gmail.com>
2022-08-18 02:08:22 -0300

    Se añade ruta /login y Login page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Suspense } from 'react';
-import './App.css';
 import { Link, Route, Switch } from "wouter"
-import Detail from './pages/Detail';
-import ErrorPage from './pages/ErrorPage';
-import SearchResults from './pages/SearchResults';
-import StaticContext from './context/StaticContext';
-import { GifsContextProvider } from './context/GifsContext';
+
+import Header from 'components/Header';
+
+import Login from 'pages/Login';
+import SearchResults from 'pages/SearchResults';
+import Detail from 'pages/Detail';
+import ErrorPage from 'pages/ErrorPage';
+
+import StaticContext from 'context/StaticContext';
+import { GifsContextProvider } from 'context/GifsContext';
+
+import './App.css';
 
 const HomePage = React.lazy(() => import('./pages/Home'))
 
-function App() {
+export default function App() {
   return (
     <StaticContext.Provider value={
       {
@@ -20,6 +26,7 @@ function App() {
       <div className="App">
         <Suspense fallback={null}>
           <section className="App-content">
+            <Header />
             <Link to="/">
               <figure className="App-logo">
                 <img alt='Giffy logo' src='/logo.png' />
@@ -38,7 +45,11 @@ function App() {
                 <Route 
                   component={Detail}
                   path="/gif/:id"
-                />            
+                />
+                <Route
+                  component={Login}
+                  path="/login"
+                />
                 <Route 
                   component={ErrorPage}
                   path="/:rest*" 
@@ -51,5 +62,3 @@ function App() {
     </StaticContext.Provider>
   );
 }
-
-export default App;

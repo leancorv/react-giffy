d66d7125eefd7cbca73b81c4108e80a92db84af0
Leandro <lecorvalan2@gmail.com>
2022-08-18 02:15:55 -0300

    UserContext - Se crea contexto

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import SearchResults from 'pages/SearchResults';
 import Detail from 'pages/Detail';
 import ErrorPage from 'pages/ErrorPage';
 
-import StaticContext from 'context/StaticContext';
+import { UserContextProvider } from 'context/UserContext';
 import { GifsContextProvider } from 'context/GifsContext';
 
 import './App.css';
@@ -17,12 +17,7 @@ const HomePage = React.lazy(() => import('./pages/Home'))
 
 export default function App() {
   return (
-    <StaticContext.Provider value={
-      {
-        name: 'midudev',
-        suscribeteAlCanal: true
-      }
-    }>
+    <UserContextProvider>
       <div className="App">
         <Suspense fallback={null}>
           <section className="App-content">
@@ -42,7 +37,7 @@ export default function App() {
                   component={SearchResults}
                   path="/search/:keyword/:rating?"
                 />
-                <Route 
+                <Route
                   component={Detail}
                   path="/gif/:id"
                 />
@@ -50,15 +45,15 @@ export default function App() {
                   component={Login}
                   path="/login"
                 />
-                <Route 
+                <Route
                   component={ErrorPage}
-                  path="/:rest*" 
+                  path="/:rest*"
                 />
               </Switch>
             </GifsContextProvider>
           </section>
         </Suspense>
       </div>
-    </StaticContext.Provider>
+    </UserContextProvider>
   );
 }
